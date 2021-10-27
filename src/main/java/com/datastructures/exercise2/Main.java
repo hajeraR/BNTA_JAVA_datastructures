95506c542b4a0eb3a542eb79c1d7e3b9a84f4bc2
Hajera Rahman <hajerarahman24@gmail.com>
2021-10-27 23:38:36 +0100

    final

--- src/main/java/com/datastructures/exercise2/Main.java
+++ src/main/java/com/datastructures/exercise2/Main.java
@@ -74,36 +74,6 @@ public class Main {
 
 
 
-//    public static boolean CheckParenthesis(String str) {
-//        if (str.isEmpty())
-//            return true;
-//
-//        Stack<Character> stack = new Stack<Character>();
-//        for (int i = 0; i < str.length(); i++) {
-//            char current = str.charAt(i);
-//            if (current == '{' || current == '(' || current == '[') {
-//                stack.push(current);
-//            }
-//
-//
-//            if (current == '}' || current == ')' || current == ']') {
-//                if (stack.isEmpty())
-//                    return false;
-//
-//                char last = stack.peek();
-//                if (current == '}' && last == '{' || current == ')' && last == '(' || current == ']' && last == '[')
-//                    stack.pop();
-//                else
-//                    return false;
-//            }
-//
-//        }
-//
-//        return stack.isEmpty();
-//    }
-
-
-
 // Stack <Pringles> pringles = new Stack<>();
 // pringles.push (new Pringles());
 //boolean empty = pringles.empty();
